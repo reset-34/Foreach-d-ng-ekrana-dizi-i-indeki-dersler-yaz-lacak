cf67133f6fd8c992d1961637e2cd6d78522f1e00
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 16:08:38 +0300

    Add project files.

---
+++ Foreach döngü ekrana dizi içindeki dersler yazılacak/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach_döngü_ekrana_dizi_içindeki_dersler_yazılacak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dersler = { "html", "css", "java", "c#" };
+
+            foreach(string yazilacak in dersler)
+            {
+                Console.WriteLine(yazilacak);                
+            }
+            Console.ReadLine();
+        }
+    }
+}
